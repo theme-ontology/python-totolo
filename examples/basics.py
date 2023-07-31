1b6341a8e5d8a9585d5d3a76dc8773687b25dcae
mikael <mikael@odinlake.net>
2023-07-31 09:34:28 +0100

    add example, profile settings, and improve performance by getting rid of deepcopy

---
+++ examples/basics.py
@@ -0,0 +1,33 @@
+import totolo
+
+
+def example():
+    #: get the latest main branch version of the ontology
+    ontology = totolo.remote()
+    print(ontology)
+    # <2945 themes, 4475 stories>
+    ontology.write("/home/mo/themes")
+    ontology = totolo.files("/home/mo/themes")
+    print(ontology)
+    # <2945 themes, 4475 stories>
+
+    #: go over all the themes
+    for theme in ontology.themes():
+        if "romantic love" in theme.name:
+            print(theme)
+    # b'personal freedom vs. romantic love'[3]
+    # b'romantic love'[3]
+
+    #: check the definition of a theme
+    love = ontology.theme["love"]
+    love.print()
+    # (...)
+
+    #: fetch a story and go over the themes therein
+    story = ontology.story["play: Macbeth (1606)"]
+    for weight, theme in story.iter_themes():
+        print(f"{weight:<10} {theme.name}")
+
+
+if __name__ == "__main__":
+    example()
