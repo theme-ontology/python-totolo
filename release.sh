a12bdfee84b14774a154fe359a6694ade924f883
odinlake <mikael@odinlake.net>
2025-05-18 10:41:24 +0100

    updated release script

--- release.sh
+++ release.sh
@@ -2,8 +2,8 @@
 set -euxo pipefail
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 if [[ "$BRANCH" != "main" ]]; then
-  echo 'Must be on main branch!';
-  exit 1;
+  echo 'Must be on main branch!'
+  exit 1
 fi
 VERSION=`python -c "import totolo; print(totolo.__version__)"`
 if [[ $VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
