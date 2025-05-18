de9b3a15538a54c9dc40a1d1a4d63195cc2f1415
odinlake <mikael@odinlake.net>
2025-05-18 10:33:56 +0100

    release script updates

--- release.sh
+++ release.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+set -euxo pipefail
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+if [[ "$BRANCH" != "main" ]]; then
+  echo 'Must be on main branch!';
+  exit 1;
+fi
 VERSION=`python -c "import totolo; print(totolo.__version__)"`
 if [[ $VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    git tag v$VERSION
-    git push origin v$VERSION
+  git pull
+  git tag v$VERSION
+  git push origin v$VERSION
 else
   echo "unexpected version format: $VERSION"
 fi
