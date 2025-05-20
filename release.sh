3f97e8ba21ac0fce636297ee8804de4f89c42145
Mikael Onsjö <mikael@odinlake.net>
2025-05-20 19:13:57 +0100

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -euo pipefail
-echo "HERE1"
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-echo "HERE2"
 if [[ "$BRANCH" != "main" ]]; then
   echo 'Must be on main branch!'
   exit 1
 fi
-echo "HERE3"
 VERSION=`python -c "import totolo; print(totolo.__version__)"`
 if [[ $VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   git pull
@@ -16,4 +13,3 @@ if [[ $VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
 else
   echo "unexpected version format: $VERSION"
 fi
-echo "HERE4"
